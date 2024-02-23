9622539bfcb08f076ca1d5d347560a47988fb325
Roy Kim <royjayshkim2@gmail.com>
2024-02-23 10:25:08 -0600

    change api name, add DB diagram

--- grouppay_app_django/grouppay_app_api/apps.py
+++ grouppay_app_django/grouppay_app_api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GrouppayAppGraphqlConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "grouppay_app_graphql"
+    name = "grouppay_app_api"
---
+++ grouppay_app_django/grouppay_app_api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+
+
+
+class 
+def __str__(self):
+    return self.step
