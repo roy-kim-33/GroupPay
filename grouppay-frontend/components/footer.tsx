9fc0b029cb771794db40c3b906547566bf1ab812
ckm67 <77990605+ckm67@users.noreply.github.com>
2024-02-25 10:59:15 -0600

    header and footer done

---
+++ grouppay-frontend/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-black">
+        <small className="mb-2 block text-xs">
+            2024 Ninjaturtles- Roy, Rachel, Connor
+        </small>
+        <p className= "text-xs">
+            <span className='font-semibold'>About this website:/</span> built with Next.js, Django, PostgreSQL
+        </p>
+    </footer>
+  )
+}
