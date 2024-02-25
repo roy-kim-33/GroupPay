c2a4d2c6722fa156773b04a6db3866b1bb073913
Roy Kim <royjayshkim2@gmail.com>
2024-02-24 21:11:41 -0600

    finish queries and add PostUser Mutation

--- grouppay_app_django/grouppay_app_api/schema.py
+++ grouppay_app_django/grouppay_app_api/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from django.http import Http404
 from django.db.models import Model, Q
+from django.contrib.auth import get_user_model
 from .models import *
 
 def get_model_or_404(model: Model, id):
@@ -12,8 +13,9 @@ def get_model_or_404(model: Model, id):
 
 class UserType(DjangoObjectType):
     class Meta:
-        model = User
-        fields = "__all__"
+        # model = User
+        # fields = "__all__"
+        model = get_user_model()
 
 class AccountType(DjangoObjectType):
     class Meta:
@@ -24,6 +26,8 @@ class PaymentStatusType(DjangoObjectType):
     class Meta:
         model = PaymentStatus
         fields = "__all__"
+    # description = graphene.String()
+        
 class GroupType(DjangoObjectType):
     class Meta:
         model = Group
@@ -36,15 +40,15 @@ class GroupMemberType(DjangoObjectType):
 
 
 
-
-#QUERIES -- GET models
+# QUERIES -- GET models
 class Query(graphene.ObjectType):
+    
     # Define queries
-    users_list = graphene.List(UserType)
-    accounts_list = graphene.List(AccountType)
-    payment_statuses_list = graphene.List(PaymentStatusType)
-    groups_list = graphene.List(GroupType)
-    group_members_list = graphene.List(GroupMemberType)
+    users_list = graphene.List(of_type=UserType, id=graphene.ID(), username=graphene.String(), email=graphene.String())
+    accounts_list = graphene.List(of_type=AccountType, id=graphene.ID(), user_id=graphene.ID())
+    payment_statuses_list = graphene.List(of_type=PaymentStatusType, id=graphene.ID(), about=graphene.String())
+    groups_list = graphene.List(of_type=GroupType, id=graphene.ID(), name=graphene.String(), leader_user_id=graphene.ID(), created_at=graphene.DateTime(), payment=graphene.Float(), status_id=graphene.ID(), about=graphene.String())
+    group_members_list = graphene.List(of_type=GroupMemberType, id=graphene.ID(), user_id=graphene.ID(), group_id=graphene.ID(), is_leader=graphene.Boolean(), accepted_payment=graphene.Boolean(), accepted_payment_at=graphene.DateTime())
     
     # Detail View
     user = graphene.Field(UserType, id=graphene.ID(required=True))
@@ -56,91 +60,92 @@ class Query(graphene.ObjectType):
     # Define query resolvers
     # NOTE TO PEERS: Currently assuming potential need to filter on all fields. This may not be necessary.
         # places to consider removing filter functionality:
-    # users: password_hash - removed
+    # users: password - removed
     # accounts: balance - removed
     # payment_statuses: description - kept for debugging
     # groups: description - removed
-    
+
     def resolve_users_list(self, info, id=None, username=None, email=None):
-        filters = {}
+        filters = Q()
         if id is not None:
-            filters['id__exact'] = id
+            filters &= Q(id=id)
         if username is not None:
-            filters['username__icontains'] = username
+            filters &= Q(username__icontains=username)
         if email is not None:
-            filters['email__icontains'] = email
-        return User.objects.filter(**filters)
+            filters &= Q(email__icontains=email)
+        return get_user_model().objects.filter(filters)
 
-
-
-    # def resolve_users_list(self, info, **kwargs):
-    # # Predefined keys that you expect in kwargs that correspond to your User model fields
-    #     valid_keys = {'id', 'username', 'email'}
-    #     # Construct filters dict dynamically, filtering out None values
-    #     # filters = {f"{key}__icontains": value for key, value in kwargs.items() if key in valid_keys and value is not None}
-    #     query_filter = Q()
-    #     for key, value in kwargs.items():
-    #         if key in valid_keys and value is not None:
-    #             query_filter &= Q(**{f'{key}__icontains': value})
-        
-        
-    #     # For exact match on 'id', adjust the filter condition
-    #     if 'id' in kwargs and kwargs['id'] is not None:
-    #         query_filter &= Q(id__exact=kwargs['id'])
-    #         # Remove the icontains filter for 'id' if it was added
-    #         query_filter &= ~Q(id__icontains=kwargs['id']) # ~ removes id__icontains from query_filter
-        
-    #     return User.objects.filter(query_filter)
-    
     
     def resolve_accounts_list(self, info, id=None, user_id=None):
-        filters = {}
+        filters = Q()
         if id is not None:
-            filters['id__exact'] = id
+            filters &= Q(id=id)
         if user_id is not None:
-            filters['user_id__exact'] = user_id
-        return Account.objects.filter(**filters)
+            filters &= Q(user_id=user_id)
+        return Account.objects.filter(filters)
           
-    def resolve_payment_statuses_list(self, info, id=None, description=None):
-        filters = {}
+    def resolve_payment_statuses_list(self, info, id=None, about=None):
+        filters = Q()
         if id is not None:
-            filters['id__exact'] = id
-        if description is not None:
-            filters['description__icontains'] = description
-        return PaymentStatus.objects.filter(**filters)  
+            filters &= Q(id=id)
+        if about is not None:
+            filters &= Q(about__icontains=about)
+        return PaymentStatus.objects.filter(filters)  
     
-    def resolve_groups_list(self, info, id=None, name=None, leader_user=None, created_at=None, payment=None, status=None, **kwargs):
-        filters = {}
+    def resolve_groups_list(self, info, id=None, name=None, leader_user_id=None, created_at=None, payment=None, status_id=None, about=None):
+        filters = Q()
         if id is not None:
-            filters['id__exact'] = id
+            filters &= Q(id=id)
         if name is not None:
-            filters['name__icontains'] = name
-        if leader_user is not None:
-            filters['leader_user__exact'] = leader_user
+            filters &= Q(name__icontains=name)
+        if leader_user_id is not None:
+            filters &= Q(leader_user_id=leader_user_id)
         if created_at is not None:
-            filters['created_at__icontains'] = created_at
+            filters &= Q(created_at__gte=created_at)
         if payment is not None:
-            filters['payment__exact'] = payment
-        if status is not None:
-            filters['status__icontains'] = status
-        return Group.objects.filter(**filters)
+            filters &= Q(payment__exact=payment)
+        if status_id is not None:
+            filters &= Q(status_id__icontains=status_id)
+        if about is not None:
+            filters &= Q(about__icontains=about)  
+        return Group.objects.filter(filters)
       
-    def resolve_group_members_list(self, info, id=None, user=None, group=None, is_leader=None, accepted_payment=None, accepted_payment_at=None):
-        filters = {}
+    def resolve_group_members_list(self, info, id=None, user_id=None, group_id=None, is_leader=None, accepted_payment=None, accepted_payment_at=None):
+        filters = Q()
         if id is not None:
-            filters['id__exact'] = id
-        if user is not None:
-            filters['user__exact'] = user
-        if group is not None:
-            filters['group__exact'] = group
+            filters &= Q(id=id)
+        if user_id is not None:
+            filters &= Q(user_id=user_id)
+        if group_id is not None:
+            filters &= Q(group_id=group_id)
         if is_leader is not None:
-            filters['is_leader__icontains'] = is_leader
+            filters &= Q(is_leader=is_leader)
         if accepted_payment is not None:
-            filters['accepted_payment__icontains'] = accepted_payment
+            filters &= Q(accepted_payment=accepted_payment)
         if accepted_payment_at is not None:
-            filters['accepted_payment_at__icontains'] = accepted_payment_at
-        return GroupMember.objects.filter(**filters)      
+            filters &= Q(accepted_payment_at__gte=accepted_payment_at)
+        return GroupMember.objects.filter(filters)      
+
+
+# MUTATIONS -- POST, PATCH, DELETE models
+class PostUser(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True) # consider using hash function here
+
+    user = graphene.Field(type_=UserType)
+    def mutate(self, info, username, email, password):
+        user = get_user_model().objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        return PostUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    post_user = PostUser.Field()
 
 
 # '''should be include Serializer
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
