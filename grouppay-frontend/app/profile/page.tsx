b9ee504bd6b426c38f030f8b283e0902bf06f2a7
rjruddy <80654885+rjruddy@users.noreply.github.com>
2024-02-26 17:11:52 -0600

    Added Profile Page

---
+++ grouppay-frontend/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import UserProfile from '../../components/UserProfile';
+
+export default function Profile() {
+    return (
+      <main className="flex flex-col items-center px-4">
+        <UserProfile/>
+      </main>
+    )
+  }
